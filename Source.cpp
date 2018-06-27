3a553f76d8e93629ba921b392919b897b891435c
Muhammad  Ehsan Khan <mkhan.bscs15seecs@nust.edu.pk>
2018-06-27 11:51:21 +0500

    Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+//void swapp(int *px, int *py)
+//{
+//	int temp;
+//	temp = *px;
+//	*px = *py;
+//	*py = temp;
+//	cout << *px << endl;
+//	cout << *py << endl;
+//
+//}
+
+int main()
+{
+	//int a = 2;
+	//int b = 3;
+	//int *c=&a;
+	//int *d=&b;
+	//int **f = &c;
+	//int g = &f;
+	//cout << a << "\n*(&a)=" << *&a<<"\n";
+	//cout<<b<<"\n&b="<<&b<<"\n";
+	//cout << "*c=" <<*c << "\n";
+	//cout << "&c=" << **&c << "\n";
+	//cout << "*d=" << *d << "\n";
+	//cout << "&d=" << &d << "\n";
+	//cout << f << endl;
+	//cout << *&f<<endl;
+	//cout << **f<<endl;
+
+	/*char c = 'a';
+	char *p = &c;
+	cout << *p<<endl;*/
+
+	//char c[2];
+	//c[0] = 'A';
+	//c[1] = '\0';
+	//cout << c << endl;
+
+	//char *months[] = { "illegal month", "jan", "feb" };
+	//cout << months[0]<<endl;
+
+
+	//return 0;
+	char a = 'a';
+	int b = 1;
+	char *c = &a;
+	int *d = &b;
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << ++c << endl;
+	cout << d << endl;
+	cout << ++d << endl;
+	
+}
+
